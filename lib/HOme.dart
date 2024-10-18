c1b9a0041e1be32ef1c760a2fd83c63ff228863a
kamelbataineh <batainehkamel2@gmail.com>
2024-10-18 18:11:49 +0300

    up

--- lib/HOme.dart
+++ lib/HOme.dart
@@ -12,17 +12,132 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Sing in"),
+        body: Stack(
+      children: [
+        Image.network(
+          "https://i.pinimg.com/enabled/564x/0d/46/5c/0d465c78df992a5acf02577d8892b9ff.jpg",
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
         ),
-        body: Column(
-          children: [
-            Username(context),
-            SizedBox(
-              width: 33,
-            ),
-            PasswordField(context),
-          ],
-        ));
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                      width: 200,
+                      height: 180,
+                      child: Image.asset("image/ll.png")),
+                ),
+              ),
+              Container(
+                  child: Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontFamily: "Trajan Pro",
+                  fontSize: 30,
+                  color: Colors.pinkAccent,
+                  fontWeight: FontWeight.w900,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1, 3),
+                      color: Colors.white.withOpacity(1),
+                    ),
+                  ],
+                ),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                // child: Container(
+                //   decoration: BoxDecoration(
+                //     gradient: LinearGradient(
+                //       colors: [
+                //         Colors.black.withOpacity(0.6), // تدرج أسود داكن
+                //         Colors.blue.withOpacity(0.4), // تدرج أزرق خفيف
+                //       ],
+                //       begin: Alignment.topCenter,
+                //       end: Alignment.bottomCenter,
+                //     ),
+                //
+                //     borderRadius: BorderRadius.circular(20),
+                //   ),
+                  child: TextFormField(
+                    decoration: decoration(
+                        lab: "username",
+                        hint: "Username,Emil,number",
+                        icon: Icons.person),
+                  ),
+                ),
+              // ),
+              SizedBox(
+                height: 0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                // child: Container(
+                //   decoration: BoxDecoration(
+                //     gradient: LinearGradient(
+                //       colors: [
+                //         Colors.black12??Colors.transparent,
+                //         Colors.blue[200] ?? Colors.transparent
+                //       ], // Gradient colors
+                //     ),
+                //     borderRadius: BorderRadius.circular(20),
+                //   ),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decoration1(
+                      lab: "password",
+                      hint: "Enter your password",
+                      icon: Icons.password,
+                    ),
+
+                  ),
+
+                ),
+              // ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black38??Colors.transparent,
+                      Colors.black38?? Colors.purple
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "login",
+
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.pinkAccent,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Trajan Pro",
+
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
--- lib/void.dart
+++ lib/void.dart
@@ -39,40 +39,123 @@ Widget Username(BuildContext context) {
 ///
 
 Widget PasswordField(BuildContext context) {
-  bool _obscureText = true;
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: TextFormField(
       keyboardType: TextInputType.text,
       obscureText: true,
       decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.cyan,
-            width: 1,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.cyan,
+              width: 1,
+            ),
           ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.blue,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.blue,
+            ),
           ),
-        ),
-        filled: true,
-        icon: Icon(Icons.lock),
-        label: Text(
-          "Password",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        fillColor: Colors.blue[100],
-        hintText: "Password",
-        hintStyle: TextStyle(
-          color: Colors.black,
-        ),
-        suffixIcon: Icon(Icons.visibility_off),
-        border:OutlineInputBorder(
-          borderRadius:BorderRadius.circular(50),
-        )
-      ),
+          filled: true,
+          icon: Icon(Icons.lock),
+          label: Text(
+            "Password",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          fillColor: Colors.blue[100],
+          hintText: "Password",
+          hintStyle: TextStyle(
+            color: Colors.black,
+          ),
+          suffixIcon: Icon(Icons.visibility_off),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+          )),
     ),
+  );
+}
+
+///
+///
+///
+///
+///
+///
+
+
+///
+///
+///
+///
+///
+///
+///
+InputDecoration decoration({
+  required String lab,
+  required String hint,
+  required IconData icon,
+}) {
+  return InputDecoration(
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.purple,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.purple,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    labelText: lab,
+    hintText: hint,
+    prefixIcon:  Icon(icon),
+    labelStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    hintStyle: TextStyle(color: Colors.black),
+    filled: true,
+    fillColor: Colors.transparent,
+  );
+}
+///
+///
+///
+InputDecoration decoration1({
+  required String lab,
+  required String hint,
+  required IconData icon,
+}) {
+  return InputDecoration(
+    helperStyle: TextStyle(color: Colors.white), // لون النص المساعد
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.purple,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.purple,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    labelText: lab,
+    hintText: hint,
+    prefixIcon:  Icon(icon),
+    labelStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    hintStyle: TextStyle(color: Colors.black),
+    filled: true,
+    fillColor:Colors.transparent,
+counterText:"Forget your password?",
+  counterStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 12,
+    fontWeight: FontWeight.w400
+
+  )
   );
 }
